3ad06938b56a715adbd5c65aecc776bca3684cbc
Stan <stanislas@gmail.com>
2021-08-19 12:33:22 +0200

    simul

---
+++ Genetik-Springboot/src/main/java/sopra/formation/rest/exception/SimulationValidationException.java
@@ -0,0 +1,9 @@
+package sopra.formation.rest.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(code = HttpStatus.BAD_REQUEST, reason = "La Simulation n'a pu être validée")
+public class SimulationValidationException {
+
+}
